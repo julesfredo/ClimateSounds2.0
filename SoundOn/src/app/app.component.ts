acbab4d0245c62c2b59f54e5f963652a248518a1
Julian Salmon <salmonjulian5@gmail.com>
2021-01-08 17:50:54 -0500

    Map displaying

--- SoundOn/src/app/app.component.ts
+++ SoundOn/src/app/app.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// declare var $: any;
 
-export class AppComponent {
+
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    // $(window)
+  }
+
   title = 'SoundOn';
+  
+  
+
+  // When the user scrolls the page, execute myFunction
+// window = function() {
+//   myFunction()
+// };
+// // Get the navbar
+//  navbar = document.getElementById("navbar");
+// // Get the offset position of the navbar
+//  sticky = navbar.offsetTop;
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
 }
--- SoundOn/src/app/map/map.component.ts
+++ SoundOn/src/app/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Loader } from "@googlemaps/js-api-loader"
 
 @Component({
   selector: 'app-map',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
+  let map: google.maps.Map;
+  const loader = new Loader({
+      apiKey: "AIzaSyDkBNtGqS7Szdp11nxG11xKhJfaiofbvG0",
+      version: "weekly"
+    });
+    
+    loader.load().then(() => {
+      map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+      });
+    });
   }
-
 }
