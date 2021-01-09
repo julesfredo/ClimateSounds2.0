5e7bd605b55a7488635bb3bc2b5064a077d85e66
Julian Salmon <salmonjulian5@gmail.com>
2021-01-09 15:21:42 -0500

    service coords set

--- SoundOn/src/app/weather-map.service.ts
+++ SoundOn/src/app/weather-map.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { ILocation } from './ILocation';
 import { OpenWeather } from './OpenWeather';
 
@@ -13,14 +13,19 @@ export class WeatherMapService {
   weatherApiKey: string = '67d4ac0e95120bc42f358dbe5cce49e8';
   openWeatherUrl: string = 'http://api.openweathermap.org/data/2.5/weather?lat=';
 
-  constructor(private http: HttpClient) { }
+  private coords = new BehaviorSubject("0,0") ;
+  currentCoord = this.coords.asObservable()
 
+
+  constructor(private http: HttpClient) { }
   getLocation(url: string):Observable<ILocation> {
       return this.http.get<ILocation>(url);
     }
     getWeather(lat: string, lng: string):Observable<OpenWeather> {
       return this.http.get<OpenWeather>(this.openWeatherUrl + lat + '&lon=' + lng + 
       '&appid=' + this.weatherApiKey);
-      
+    }
+    setCoord(coordinates:string) {
+      this.coords.next(coordinates);
     }
 }
